eaeb6a04b8e63c1e1d1bf5c8fb2d126b2c593aca
Arjun Pandiyan <apandiyan@zynga.com>
2014-01-21 13:55:09 -0800

    Initial Commit

---
+++ TemperatureConverter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TemperatureConverter
+//
+//  Created by Arjun Pandiyan on 1/21/14.
+//  Copyright (c) 2014 Arjun Pandiyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TemperatureConverterTests/TemperatureConverterTests.h
@@ -0,0 +1,13 @@
+//
+//  TemperatureConverterTests.h
+//  TemperatureConverterTests
+//
+//  Created by Arjun Pandiyan on 1/21/14.
+//  Copyright (c) 2014 Arjun Pandiyan. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface TemperatureConverterTests : SenTestCase
+
+@end
